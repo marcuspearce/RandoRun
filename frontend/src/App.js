71ca5b9dab7ea21694bd9510b1d1ee64b297ae67
Marcus Pearce <marcuspearce7@gmail.com>
2021-03-26 00:58:05 -0700

    add frontend to firebase project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import firebase from "firebase";
 
 function App() {
+  const firebaseApp = firebase.apps[0];
   return (
     <div>
+      <h1>Hello hello</h1>
+      <code>
+        <pre>{JSON.stringify(firebaseApp.options,null,2)}</pre>
+      </code>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from "firebase/app";
+
+import configData from "./config.json"
+
+firebase.initializeApp({
+  apiKey: configData.API_KEY,
+  authDomain: "randorun-f1a37.firebaseapp.com",
+  databaseURL: "https://randorun-f1a37-default-rtdb.firebaseio.com",
+  projectId: "randorun-f1a37",
+  storageBucket: "randorun-f1a37.appspot.com",
+  messagingSenderId: "217611233215",
+  appId: "1:217611233215:web:b2ac55e6ef1366183e630a",
+  measurementId: "G-1FEXJYLNK2"
+});
 
 ReactDOM.render(
   <React.StrictMode>
